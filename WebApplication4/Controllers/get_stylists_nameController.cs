286b6f5e8086199c3a30f580ceec0207dae824c4
shehan1993 <shehan1993@gmail.com>
2018-01-04 09:13:57 +0530

    second commit

---
+++ WebApplication4/Controllers/get_stylists_nameController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Web.Http;
+using System.Configuration;
+
+namespace WebApplication4.Controllers
+{
+    public class datas
+    {
+        public string id;
+        public string first_name;
+        public string last_name;
+        public string address_line_01;
+        public string address_line_02;
+        public string city;
+        public string state;
+        public string zip;
+        public string country;
+        public string Telephone;
+        public string description;
+        public string term_and_conditions;
+        public string charge_per_slot;
+        public string job_role_id;
+    }
+
+
+    public class get_stylists_nameController : ApiController
+    {
+         // GET api/get_stylists_name
+        public List<string> GetAllStylistsName()
+        {
+            List<string> names = new List<string>();
+            List<string> ids = new List<string>();
+
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+
+            string[] x = new string[7];
+            
+            using (SqlConnection con = new SqlConnection(CS))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("SELECT name,id FROM trn_User_Account", con);
+
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+
+                    Debug.WriteLine(rdr["name"]);
+                    names.Add(rdr["name"].ToString());
+                    names.Add(rdr["id"].ToString());
+                   
+
+                                     
+                }
+            }
+
+            
+
+            return names;
+          
+        }
+
+        // GET api/get_stylists_name
+        public List<object> GetAllStylistsName(int id)
+        {
+            List<object> details = new List<object>();
+            int job_role_id=0;
+
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+
+            string[] x = new string[7];
+
+            datas d = new datas();
+            using (SqlConnection con = new SqlConnection(CS))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("SELECT * FROM trn_stylist WHERE ID = '"+ id +"'", con);
+
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    
+                    d.id = rdr["id"].ToString();
+                    d.first_name = rdr["first_name"].ToString();
+                    d.last_name = rdr["last_name"].ToString();
+                    d.address_line_01 = rdr["address_line_01"].ToString();
+                    d.address_line_02 = rdr["address_line_02"].ToString();
+                    d.city = rdr["city"].ToString();
+                    d.state = rdr["state"].ToString();
+                    d.zip = rdr["zip"].ToString();
+                    d.country  = rdr["country"].ToString();
+                    d.Telephone = rdr["Telephone"].ToString();
+                    d.description = rdr["description"].ToString();
+                    d.term_and_conditions = rdr["term_and_conditions"].ToString();
+                    d.charge_per_slot = rdr["charge_per_slot"].ToString();
+                    job_role_id = Convert.ToInt32(rdr["job_role_id"]);
+                    
+
+                }
+
+
+            }
+
+
+            using (SqlConnection con = new SqlConnection(CS))
+            {
+                con.Open();
+
+                SqlCommand cmd = new SqlCommand("SELECT role FROM trn_job_role WHERE id = '" + job_role_id+"'", con);
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+
+
+                    d.job_role_id =rdr["role"].ToString();
+
+                }
+            }
+
+            details.Add(d);
+            return details;
+
+        }
+
+
+
+
+    }
+}
---
+++ WebApplication4/Controllers/mark_as_busyController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Web.Http;
+using System.Configuration;
+namespace WebApplication4.Controllers
+{
+
+    public class dates {
+        public Int32 year;
+        public Int32 month;
+        public Int32 day;
+        public Int32 time;
+        public Int32 type;
+        
+    }
+
+    public class mark_as_busyController : ApiController
+    {
+
+        // GET api/get_stylists_name
+        public List<object> GetBusyDates(int id)
+        {
+            List<object> detail = new List<object>();
+            int job_role_id = 0;
+
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+
+            string[] x = new string[7];
+
+            using (SqlConnection con = new SqlConnection(CS))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("SELECT * FROM trn_busy_date WHERE stylist_id = '" + id + "'", con);
+
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                 
+                 
+                    dates d = new dates();
+                    d.time = Int32.Parse(rdr["time_slot_id"].ToString());
+                    d.time = 35;
+                    d.type = Int32.Parse(rdr["type"].ToString());
+                    string s = rdr["date"].ToString();
+                    s = s.Replace(" 12:00:00 AM", "");
+                    string[] words = s.Split('/');
+                    d.day = Int32.Parse(words[1]);
+                    d.month = Int32.Parse(words[0]);
+                    d.year = Int32.Parse(words[2]);
+
+
+                    detail.Add(d);
+
+                }
+
+
+            }
+
+
+            return detail;
+
+        }
+
+    }
+}
